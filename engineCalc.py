975b9788682424d2f676ad8399d8318d8307396f
Salacious765 <acooper98@homail.com>
2024-10-03 11:11:45 +0100

    Removed old code and semicolon at start

--- engineCalc.py
+++ engineCalc.py
@@ -1,4 +1,4 @@
-;import sys
+import sys
 import math
 import csv
 
@@ -113,59 +113,3 @@ def cc(Vd, cyl):
 engine = selectEngine(data, selected)
 completeEngine(engine)
 
-
-'''
-#Define variables for maths equations based on selected engine
-Model = (engine[selected][iMODEL]) #Model Name
-B = float(engine[selected][iB]) #Bore
-L = float(engine[selected][iL]) #Stroke
-cyl = int(engine[selected][iCYL]) #Cyl No
-Vd = float(engine[selected][iVD]) #Swept Displacement
-cc = float(engine[selected][iCC]) #Total swept displacement
-Vc = float(engine[selected][iVC]) #Clearance volume
-rc = float(engine[selected][RC]) #Compression Ratio
-
-
-#cc = cc(Vd, cyl)
-#rc = rc(Vd, Vc) 
-
-#Print obtained data for selected engine
-print(Model, B, L, cyl,Vd, round(cc,3), Vc, round(rc,1))
-
-
-#Old code for intersecting the data with a dictionary.
-#Import data and populate dictionary
-columnHeaders = (Name, Bore, Stroke, Cylinders, compressionRatio, clearanceVolume)
-#Define dictionary column names
-Name = 'Name' #Expect string name
-Bore = "Bore" #Expect cc in cm
-Stroke = "Stroke" #Expect stroke in cm
-Cylinders = "Cylinders" #Expect number of cylinders in count
-compressionRatio = "Compression Ratio" #Expect CR as 1pd 12.1
-clearanceVolume = "Clearance Volume" #Expect clearance volume in cm^3
-
-#Create dictionary
-engine = {Name:[], Bore:[], Stroke:[], Cylinders:[], compressionRatio:[], clearanceVolume:[]}
-data = csv.reader(open(sys.argv[1]))
-
-for row in data:
-    for column in columnHeaders:
-        for value in row:
-            engine[column].append(value)
-            print(engine)
-
-engine[Name].append(row[0])
-engine[Bore].append(row[1])
-engine[Stroke].append(row[2])
-engine[Cylinders].append(row[3])
-engine[compressionRatio].append(row[4])
-engine[clearanceVolume].append(row[5])
-
-#Show entire dictionary
-print(engine[Name])
-print(engine[Bore])
-print(engine[Stroke])
-print(engine[Cylinders])
-print(engine[compressionRatio])
-print(engine[clearanceVolume])
-'''
